8aaccbb9184d255998041a53447d32fe35f9179a
krillinkills <beingvaibhav858@gmail.com>
2021-12-22 22:26:12 +0530

    created measuring hand distance

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import math
+import numpy as np
+import cvzone
 
 capture = cv2.VideoCapture(0)
 capture.set(3, 1280)
@@ -8,18 +10,26 @@ capture.set(4, 720)
 
 #Hand detector
 detector = HandDetector(detectionCon=0.8,maxHands=1)
+x = [300,245,200,170,145,130,112,103,93,87,80,75,70,67,59,57]
+y = [20,25,30,35,40,45,50,55,60,65,70,80,85,90,95,100]
+
+coff = np.polyfit(x,y,2)
 
 while True:
     success, img = capture.read()
-    hands, img = detector.findHands(img)
+    hands= detector.findHands(img,draw=False)
 
     if hands:
         lmLists = hands[0]['lmList']
+        x,y,w,h=hands[0]['bbox']
         x1, y1 = lmLists[5]
         x2, y2 = lmLists[17]
         distance = int(math.sqrt((y2-y1)**2 + (x2-x1)**2))
+        A,B,C= coff
+        distanceCM=A*distance**2+B*distance+C
 
-        print(abs(x2-x1), distance)
+        #print(distance,distanceCM)
+        cvzone.putTextRect(img,f'{int(distanceCM)}cm',(x,y))
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
